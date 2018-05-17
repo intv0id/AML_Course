540a4540924cc7037517c026859f3a28d2ff9d9b
Clément Trassoudaine <clement.trassoudaine@eurecom.fr>
2018-05-17 12:41:49 +0200

    link correction

--- description_generator.sh
+++ description_generator.sh
@@ -9,7 +9,7 @@ function titled_desc {
         notebook_file=$(echo $notebook | sed "s/.*\/\(.*\)$/\1/")
         description_file="$notebook_path/description.md"
         buffer_description_file="$notebook_path/buffer_description.md" 
-        html_notebook_file=$(echo $notebook | sed "s/\(.*\).ipynb/\1.html/")
+        html_notebook_file=$(echo $notebook_file | sed "s/\(.*\).ipynb/\1.html/")
        
 
         echo -e "\n## [$notebook_file]($html_notebook_file)\n" > $buffer_description_file
